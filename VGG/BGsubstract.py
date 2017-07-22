9185e4dee67564910a21fde133df99cb0bcccf2d
Pierre Baqué <pierre.baque@polytechnique.edu>
2017-07-21 16:36:16 -1000

    Update BGsubstract.py

--- VGG/BGsubstract.py
+++ VGG/BGsubstract.py
@@ -8,7 +8,6 @@ from PIL import Image, ImageDraw
 import theano
 import theano.tensor as T
 from theano.tensor.nnet.conv import conv2d
-from theano.tensor.signal.downsample import max_pool_2d
 from theano.tensor.shared_randomstreams import RandomStreams
 
 class BGsubstract(object):
