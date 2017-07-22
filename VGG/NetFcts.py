37bcf41254042e224048dca2a0024ba0999e94db
Pierre Baqué <pierre.baque@polytechnique.edu>
2017-07-21 16:35:36 -1000

    Update NetFcts.py

--- VGG/NetFcts.py
+++ VGG/NetFcts.py
@@ -3,7 +3,6 @@ import numpy as np
 import theano
 import theano.tensor as T
 from theano.tensor.nnet.conv import conv2d
-from theano.tensor.signal.downsample import max_pool_2d
 
 #define generic functions
 def toShared(x):
